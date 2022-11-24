6dceb025be5305c192ccb6f92724b9195e8d48e4
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-25 00:23:17 +0600

    created  productCard based on specific product

--- client/src/Pages/CategoryProducts/ProductCard/ProductCard.js
+++ client/src/Pages/CategoryProducts/ProductCard/ProductCard.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ProductCard.css';
+import { MdVerifiedUser } from 'react-icons/md';
 
 const ProductCard = ({ product }) => {
   const {
@@ -12,11 +13,58 @@ const ProductCard = ({ product }) => {
     postedTime,
     sellerName,
     verified,
+    mobileNumber,
   } = product;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
   return (
     <div>
-      <h5>Product Card</h5>
+      <div className="card mb-3 p-4 border-0 shadow">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={image} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <p className="card-text mb-1">
+                <small className="text-muted">
+                  Posted on {postedTime}, {location}
+                </small>
+              </p>
+              <p className="card-text mb-1">
+                <strong>Resale Price:</strong>&nbsp;
+                <span className="text-success fw-bold">{resalePrice} TK</span>
+              </p>
+              <p className="card-text mb-1">
+                Original Price:
+                <span className="text-success"> {originalPrice} TK</span>
+              </p>
+              <p className="card-text mb-1">Used: {yearsOfUse}</p>
+              <div className="d-flex gap-3 align-items-center">
+                <p className="card-text mb-1">Seller: {sellerName}</p>
+                {verified && (
+                  <p
+                    style={{ color: 'blue', fontSize: '11.6px' }}
+                    className="mb-1 fw-semibold"
+                  >
+                    <MdVerifiedUser size={20} />
+                    <small> VERIFIED SELLER</small>
+                  </p>
+                )}
+              </div>
+              <p className="card-text mb-1">Contact: {mobileNumber}</p>
+
+              <button type="button" className="btn btn-info mt-4">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
