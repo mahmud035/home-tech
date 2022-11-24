e78eeb2cb29ccef94c94c9376b627a4e11be1b45
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-25 00:35:53 +0600

    make product card responsive

--- client/src/Pages/CategoryProducts/CategoryProducts/CategoryProducts.js
+++ client/src/Pages/CategoryProducts/CategoryProducts/CategoryProducts.js
@@ -11,7 +11,7 @@ const CategoryProducts = () => {
     <div className="container">
       <h2 className="text-center py-5">All Products</h2>
 
-      <div className="product-card-container">
+      <div className="product-card-container pb-5">
         {categoryProducts.map((product, index) => (
           <ProductCard key={index} product={product}></ProductCard>
         ))}
