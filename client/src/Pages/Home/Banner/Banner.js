7491a2b51d443417808c59462c50a889b8c87500
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-24 16:49:09 +0600

    added banner section

--- client/src/Pages/Home/Banner/Banner.js
+++ client/src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Banner.css';
+import Carousel from 'react-bootstrap/Carousel';
+import banner from '../../../assets/images/banner.webp';
+import banner2 from '../../../assets/images/banner2.png';
+import banner3 from '../../../assets/images/banner3.PNG';
 
 const Banner = () => {
   return (
-    <div>
-      <h3>Banner</h3>
+    <div className="carousel-container">
+      <Carousel fade>
+        <Carousel.Item>
+          <div className="carousel-img">
+            <img className="d-block " src={banner} alt="First slide" />
+          </div>
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-img">
+            <img className="d-block" src={banner2} alt="Second slide" />
+          </div>
+
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-img">
+            <img className="d-block " src={banner3} alt="Third slide" />
+          </div>
+
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
