354c896945cfeefc0ebdd7929d915c34bee0cd9c
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-27 09:44:15 +0600

    verify seller through Admin

--- client/src/Pages/Dashboard/AllSellersCard/AllSellersCard.js
+++ client/src/Pages/Dashboard/AllSellersCard/AllSellersCard.js
@@ -4,9 +4,24 @@ import { FaUser } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 import { RiDeleteBin2Fill } from 'react-icons/ri';
 import { toast } from 'react-toastify';
+import { Button } from 'react-bootstrap';
+import { MdVerifiedUser } from 'react-icons/md';
 
 const AllSellersCard = ({ seller, refetch }) => {
   const { _id, name, image, email } = seller;
+
+  const handleVerifySeller = (id) => {
+    fetch(`http://localhost:5000/sellers/${id}`, {
+      method: 'PUT',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success(`Seller ${name} verified successfully`);
+          refetch();
+        }
+      });
+  };
 
   const handleDeleteSeller = (id) => {
     fetch(`http://localhost:5000/sellers/${id}`, {
@@ -30,6 +45,11 @@ const AllSellersCard = ({ seller, refetch }) => {
           <p className="mb-2 d-flex align-items-center">
             <FaUser size={16} className="me-2" />
             <strong>{name.toUpperCase()}</strong>
+            {seller?.verified && (
+              <p style={{ color: 'blue' }} className="mb-1 ms-1 fw-semibold">
+                <MdVerifiedUser size={16} />
+              </p>
+            )}
           </p>
           <p>
             <MdEmail size={16} className="me-1" /> {email}
@@ -42,6 +62,16 @@ const AllSellersCard = ({ seller, refetch }) => {
         onClick={() => handleDeleteSeller(_id)}
         className="delete-icon"
       />
+
+      <Button
+        onClick={() => handleVerifySeller(_id)}
+        disabled={seller?.verified ? true : false}
+        className="mt-4 "
+        variant="primary"
+        size="sm"
+      >
+        {seller?.verified ? 'Verified' : 'Verify Seller'}
+      </Button>
     </div>
   );
 };
